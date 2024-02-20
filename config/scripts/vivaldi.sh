62996cfc87e9ae989610f9189197c22ce48fc5dc
Scott Ames <scott@ames.sh>
2024-02-19 17:31:57 -0800

    refactor(vivaldi): skip install

--- config/scripts/vivaldi.sh
+++ config/scripts/vivaldi.sh
@@ -3,14 +3,16 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "-- Touching up Vivaldi, assuming rpm installed --"
+echo "-- Skipping Vivaldi... --"
 
-rm /usr/bin/vivaldi-stable
-ln -s /usr/lib/opt/vivaldi/vivaldi /usr/bin/vivaldi-stable
-
-# Assumes Google Chrome installed via Flatpak
-rm /usr/lib/opt/vivaldi/WidevineCdm
-ln -s /var/lib/flatpak/app/com.google.Chrome/current/active/files/extra/WidevineCdm \
-  /usr/lib/opt/vivaldi/WidevineCdm
+# echo "-- Touching up Vivaldi, assuming rpm installed --"
+#
+# rm /usr/bin/vivaldi-stable
+# ln -s /usr/lib/opt/vivaldi/vivaldi /usr/bin/vivaldi-stable
+#
+# # Assumes Google Chrome installed via Flatpak
+# rm /usr/lib/opt/vivaldi/WidevineCdm
+# ln -s /var/lib/flatpak/app/com.google.Chrome/current/active/files/extra/WidevineCdm \
+#   /usr/lib/opt/vivaldi/WidevineCdm
 
 echo "---"
